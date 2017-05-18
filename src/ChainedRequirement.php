61bd62758f710a54c795c6661b4ad24868f7bd32
unwarysheep <unwarysheep@icloud.com>
2017-05-18 15:06:50 +0100

    Add ChainedRequirement.php

---
+++ src/ChainedRequirement.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Requirements Checker library.
+ *
+ * © Simon Deeley <s.deeley@icloud.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aside\RequirementsChecker;
+
+use Aside\RequirementsChecker\Exception\RequirementException;
+
+trait ChainedRequirement
+{
+    /**
+     * @var array
+     */
+    private $requirements = [];
+    
+    /**
+     * Error message if requirement is not met
+     *
+     * @return  string
+     */
+    public function __toString()
+    {
+        return '';
+    }
+    
+    /**
+     * Check that the requirements are met
+     *
+     * @return  bool                    Returns true on success
+     * @throws  RequirementException    if requirements are not met
+     */
+    final public function check()
+    {        
+        foreach($this->requirements as $requirement) {
+            $requirement->check();
+        }
+
+        if ($this->evaluate() === true) {
+            return true;
+        }
+        
+        throw new RequirementException($this);
+    }
+    
+    /**
+     * Add a new requirement
+     *
+     * @param   Requirement $requirement
+     * @return  self
+     */
+    final public function add(Requirement $requirement)
+    {
+        $this->requirements[] = $requirement;
+        
+        return $this;
+    }
+}
