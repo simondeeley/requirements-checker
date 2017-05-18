fa41eb79e44d12e5dd88bbeadaa434daf971b310
unwarysheep <unwarysheep@icloud.com>
2017-05-18 15:07:02 +0100

    Add RequirementException.php

---
+++ src/Exception/RequirementException.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the Requirements Checker library.
+ *
+ * © Simon Deeley <s.deeley@icloud.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Aside\RequirementsChecker\Exception;
+
+class RequirementException extends \Exception {}
